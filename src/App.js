d3e1a4e7a0b74f358646b37d940efb08ec0c678c
yitiaoshayu <695622645@qq.com>
2021-11-16 17:04:10 +0800

    feat: update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Game from './game/index'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        <div className={"breath"}>❤</div>
         <p>
           ❤我爱豆豆❤
         </p>
+          <Game/>
       </header>
     </div>
   );
---
+++ src/game/index.jsx
@@ -0,0 +1,35 @@
+import './index.css'
+import CDR from './吃豆人.png'
+import Dou from './豆.png'
+import {useState, useEffect} from "react";
+
+const Game = () => {
+    const [left, setLeft] = useState(5)
+    const [food, setFood] = useState([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+    useEffect(() => {
+        let l = left
+        let f = food
+        f = f.slice(0, f.length - 1)
+        l += 10
+        if (l > 95) {
+            l = 5
+            f = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        }
+        setTimeout(() => {
+            setLeft(l)
+            setFood(f)
+        }, 1000)
+    }, [left, food])
+
+    return(
+        <div className={"game-container"}>
+            <img className={"game-body"} style={{left: `${left}%`}} src={CDR} alt=""/>
+            {food.map((e, index) => {
+                    return <img src={Dou} style={{right: `${2.8 + 10 * index}%`}} key={index} className={"game-circle"}></img>
+
+            })}
+        </div>
+    )
+}
+
+export default Game
